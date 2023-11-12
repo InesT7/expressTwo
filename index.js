254388ca01838dd82e922e4b6f88e34b86920dc5
InesT7 <mitorres@gus.pittstate.edu>
2023-11-12 17:44:26 -0600

    testing

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-// run `node index.js` in the terminal
+var express = require('express');
+var app = express();
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+app.get('/', function (req, res) {
+res.send('Hello World');
+})
+
+app.get('/hello', function (req, res) {
+    res.send('Hello World, this is a test');
+    })
+
+
+var server = app.listen(7000, function () {
+var host = server.address().address
+var port = server.address().port
+console.log("Example app listening at http://%s:%s", host, port)
+})
