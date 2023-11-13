7f5e004fa8b55307f9f114aedbb89418be07884c
InesT7 <mitorres@gus.pittstate.edu>
2023-11-12 22:25:12 -0600

    installExpressGenerator

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-var express = require('express');
-var app = express();
-
-app.get('/', function (req, res) {
-res.send('Hello World');
-})
-
-app.get('/hello', function (req, res) {
-    res.send('Hello World, this is a test');
-    })
-
-
-var server = app.listen(7000, function () {
-var host = server.address().address
-var port = server.address().port
-console.log("Example app listening at http://%s:%s", host, port)
-})
+console.log ('Hi');
